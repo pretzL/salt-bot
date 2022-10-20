6dd2e7a6872213541057103bb97217fedc70b677
pretzL <ericpretzinger@gmail.com>
2022-10-20 20:26:36 +0200

    Test adding secret key via Git

--- test.js
+++ test.js
@@ -2,5 +2,9 @@ require("dotenv/config");
 
 function test(expectedSecret) {
   const secretMatches = process.env.BOT_KEY == expectedSecret;
+  console.log(`The Bot key is ${process.env.BOT_KEY}`);
+  console.log(`The secret does${secretMatches ? "" : " not"} match!`);
   return secretMatches;
 }
+
+test();
